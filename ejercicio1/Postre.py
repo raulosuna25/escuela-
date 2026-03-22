64ec73d5d1ce234f6026bfe211057cfe073e5a7b
Raul osuna <alC22760487@ite.edu.mx>
2026-03-21 17:56:05 -0700

    Add Postre class inheriting from Platillo

---
+++ ejercicio1/Postre.py
@@ -0,0 +1,11 @@
+from Platillo import Platillo
+
+class Postre(Platillo):
+
+    def __init__(self, nombre, precio, es_sin_gluten):
+        super().__init__(nombre, precio)
+        self.es_sin_gluten = es_sin_gluten
+
+    def tipo(self):
+        gluten = "Sí" if self.es_sin_gluten else "No"
+        print(f"Tipo: Postre — Sin gluten: {gluten}")
